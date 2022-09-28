36e1eda359ba5611106967077bf0f13667820877
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-28 13:36:03 +0530

    Added:Usecase 1 for day 19Problems

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,6 +3,9 @@
  */
 package com.bridgelabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Iam_A
  *
@@ -10,10 +13,35 @@ package com.bridgelabz;
 public class UserRegistration {
 
 	/**
-	 * @param args
+	 * UC1
+	 * As a User need to enter a valid First Name
+     * First name starts with Cap and has minimum 3 characters
 	 */
+	
+	
+		public static boolean checkFirstName(String fName) {
+			String  userRegistrationRegEx = "[A-Z]{1}[a-z]{2,6}";
+			Pattern patternObj = Pattern.compile(userRegistrationRegEx);
+			if(fName == null) {
+				return false;
+			}
+			Matcher matcherObj = patternObj.matcher(fName);
+			
+			return matcherObj.matches();
+			
+		}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("---------UserRegistration-------------");
+		
+		String fName ="Ananth";
+		boolean isfName = checkFirstName(fName);
+		if(isfName)
+			System.out.println(fName+"is an vaild FirstName");
+		else
+			System.out.println(fName+"is an Invaild FirstName");
+		
 
 	}
 
